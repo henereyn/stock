d38ba5f51cbc1068b39c1e5c2abaf72a8333928e
henereyn <henereyn@gmail.com>
2025-09-23 18:25:01 +0600

    news fix 1

--- app.js
+++ app.js
@@ -22,23 +22,46 @@ const avUrl = "//www.alphavantage.co/query?function=GLOBAL_QUOTE&amp;symbol=";
 async function fetchNewsData(){
     // const keyword = "Apple";
     const keyword = document.getElementById("stockName").innerText;
-    const newsUrl = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${newsKey}&language=ru&pageSize=1`;
+    const newsUrl = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${newsKey}&language=en&pageSize=10`;
     
     const newsResponse = await fetch(newsUrl);
     const news = await newsResponse.json();
-    document.getElementById('newsTitle').innerText = news.articles['0'].title;
-    document.getElementById('newsDesc').innerText = news.articles['0'].description;
-    // console.log(news.articles['0'].title,news.articles['0'].description);
-};
-
-const detailsElem = document.querySelector('.details');
-document.querySelector('.table__row').addEventListener('click',function()
+    const newsWrapper = document.getElementById("newsWrapper");
+    let newsToWrap = '';
+    for(let i = 0; i<10;i++)
     {
-        this.classList.toggle('active');
-        detailsElem.style.display = 'block';
-        fetchNewsData();
+        newsToWrap += `<div class="details__news-title"><h3>` + news.articles[i].title + `</h3></div>
+        <div class="details__news-desc">`+ news.articles[i].description +`</div>
+        <div class="details__news-date"><small>`+ news.articles[i].publishedAt +`</small></div><hr>`
     }
-);
+    newsWrapper.innerHTML = newsToWrap;
+    // console.log(news.articles[0].title,news.articles[0].description);
+};
+var detailsElem = document.getElementById('details');
+var detailsOpen = document.getElementsByClassName('table__row');
+
+for(let i = 0;i<detailsOpen.length;i++)
+    {
+        detailsOpen[i].addEventListener('click',function()
+        {
+            openDetails();
+        }
+    )};
+
+function openDetails(){
+    detailsElem.style.display = 'block';
+    fetchNewsData();
+}
+function closeDetails(){
+    detailsElem.style.display = 'none';
+}
+// document.querySelector('.table__row').addEventListener('click',function()
+//     {
+//         this.classList.toggle('active');
+//         detailsElem.style.display = 'block';
+//         fetchNewsData();
+//     }
+// );
 document.querySelector('.details__close').addEventListener('click',function()
     {
         detailsElem.style.display = 'none';
