aeb106fe42847ae5c7b9b079f0e3a0a0d98976ac
henereyn <henereyn@gmail.com>
2025-09-26 00:19:28 +0600

    first chart(per day)

--- app.js
+++ app.js
@@ -3,31 +3,73 @@ const avKey = 'JG5BTF4B8AXHCCKI';
 const newsKey = "203a3bbc12ff4ff18ffdfce847b6f27a";
 // const avUrl = "//www.alphavantage.co/query?function=GLOBAL_QUOTE&amp;symbol=";
 // const newsUrl = "https://newsapi.org/v2/everything?q="
-
+async function getStockSymbols(){
+    const ibmSymbol = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=${avKey}`;
+    const ibmResponse = await fetch(ibmSymbol);
+    const ibmData = await ibmResponse.json();
+    const ibm = ibmData["Global Quote"];
+    document.getElementById("stockSymbol").innerText = ibm["01. symbol"];
+    document.getElementById("stockPrice").innerText = ibm["05. price"]
+    document.getElementById("stockChange").innerText = ibm["09. change"]+' '+ibm["10. change percent"];
+    document.getElementById("stockVolume").innerText = ibm["06. volume"];
+}
+getStockSymbols();
 async function fetchSymbolkData() {
     // const symbol =  "Apple";
     const symbol = document.getElementById("stockSymbol").innerText;
-    // const symbol =  document.getElementById("stockSymbol");
-    // document.getElementById("symbolShow").innerText = symbol.value;
-    // const globalQuotekUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${avKey}`;
-    // const globalQuoteResponse = await fetch(globalQuotekUrl);
-    // const globalQuoteData = await globalQuoteResponse.json();
-    // const globalQuote = globalQuoteData["Global Quote"];
-    // document.getElementById("price").innerText = globalQuote["05. price"]
-    // document.getElementById("change").innerText = globalQuote["09. change"]
-    // document.getElementById("open").innerText = globalQuote["02. open"]
-    // document.getElementById("high").innerText = globalQuote["03. high"]
-    // document.getElementById("low").innerText = globalQuote[ "04. low"]
-    // document.getElementById("volume").innerText = globalQuote["06. volume"]
+    const globalQuotekUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${avKey}`;
+    const globalQuoteResponse = await fetch(globalQuotekUrl);
+    const globalQuoteData = await globalQuoteResponse.json();
+    const globalQuote = globalQuoteData["Global Quote"];
+    document.getElementById("price").innerText = globalQuote["05. price"]
+    document.getElementById("change").innerText = globalQuote["09. change"]
+    document.getElementById("open").innerText = globalQuote["02. open"]
+    document.getElementById("high").innerText = globalQuote["03. high"]
+    document.getElementById("low").innerText = globalQuote[ "04. low"]
+    document.getElementById("volume").innerText = globalQuote["06. volume"]
+
+    // console.log(news.articles[0].title,news.articles[0].description);
+};
     // document.getElementById('stockData').innerHTML = stock;
     // const stockUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${avKey}`;
     // const stockResponse = await fetch(stockUrl);
     // const stockData = await stockResponse.json();
     // console.log(stockData)
-}
+// }
+async function stockChartDay() {
+    const chartUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTERDAY&symbol=IBM&interval=60minapikey=${avKey}`;
+    const chartUrlResponse = await fetch(chartUrl);
+    const chartData = await chartUrlResponse.json();
+    // const chart = chartData["Time Series (60min)"];
+    // var today = new Date();
+    // var dd = String(today.getDate()-1).padStart(2, '0');
+    // var mm = String(today.getMonth() + 1).padStart(2, '0');
+    // var yyyy = today.getFullYear();
+    // today = yyyy + '-' + mm + '-' + dd;
+    const chartDataset = Object.values(chart).map(c => c["4. close"]).toReversed()
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: [],
+        datasets: [{
+            label: '',
+            data: chartDataset,
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+}   
 async function fetchNewsData(){
     // const keyword = "Apple";
-    const keyword = document.getElementById("stockName").innerText;
+    const keyword = document.getElementById("stockSymbol").innerText;
     const newsUrl = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${newsKey}&language=en&pageSize=10`;
     
     const newsResponse = await fetch(newsUrl);
@@ -43,8 +85,7 @@ async function fetchNewsData(){
         </div>`
     }
     newsWrapper.innerHTML = newsToWrap;
-    // console.log(news.articles[0].title,news.articles[0].description);
-};
+}  
 var detailsElem = document.getElementById('details');
 var detailsOpen = document.getElementsByClassName('table__row');
 var overlay = document.getElementById('overlay')
